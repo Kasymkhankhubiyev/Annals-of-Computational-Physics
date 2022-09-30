745367ff12a5c97e5ae14f3931d0cb5ac3add78d
Kaseke <kasymkhankhubievnis@gmail.com>
2022-09-30 13:18:07 +0700

    commit changes

--- task3.py
+++ task3.py
@@ -9,21 +9,28 @@ a1, b1, a2, b2 = 1, -1, 0, 1
 N = 10
 
 def run():
+    print('Считаем первый интеграл:')
     print('trapeze method: \n')
-    trapeze_method(a1, b1, N)
-    print('\n\n')
+    trapeze_method(a1, b1, N, f1)
+    print('\n')
     print('Simpson method: \n')
-    simpson_method(a1, b1, N)
+    simpson_method(a1, b1, N, f1)
+    print('\n\nСчитаем второй интеграл:')
+    print('trapeze method: \n')
+    trapeze_method(a2, b2, N, f2)
+    print('\n')
+    print('Simpson method: \n')
+    simpson_method(a2, b2, N, f2)
 
 def f1(x):
     y = 1. / (1. + x * x)
     return y
 
 def f2(x):
-    y = pow(x, 1/3) * np.exp(np.sin(x))
+    y = x ** (1./3.) * np.exp(np.sin(x))
     return y
 
-def trapeze_method(a, b, N):
+def trapeze_method(a, b, N, f):
     """
     S[a,b]=(f(a) + f(b))/2 * (b - a)
     :return:
@@ -31,14 +38,14 @@ def trapeze_method(a, b, N):
     for n in range(1, N):
         xarr = np.linspace(a, b, 2 ** n)
         h = xarr[1] - xarr[0]
-        yarr = [f1(x) for x in xarr]
+        yarr = [f(x) for x in xarr]
         S = 0
         for y in yarr:
             S += y
         S = (S - 0.5*(yarr[0] + yarr[2**n-1]))*h
         print(f' Кол-во разбиений:\t {2**n} \t\t Значение интеграла:\t{S}')
 
-def simpson_method(a, b, N):
+def simpson_method(a, b, N, f):
     """
     S[a,b] = (b - a)/6 * [f(a) +4f((a+b)/2) + f(b)]
 
@@ -48,7 +55,7 @@ def simpson_method(a, b, N):
     for n in range(1, N):
         xarr = np.linspace(a, b, 2 ** n)
         h = xarr[1] - xarr[0]
-        yarr = [f1(x) for x in xarr]
+        yarr = [f(x) for x in xarr]
         S = 0
         for i in range(len(yarr)):
             if i%2 == 0:
