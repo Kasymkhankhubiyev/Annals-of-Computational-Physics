c1b67d48beacd0c59ac1108187617b8fb27ffa31
Kaseke <kasymkhankhubievnis@gmail.com>
2022-10-07 13:49:37 +0700

    commit task6

---
+++ task6.py
@@ -0,0 +1,36 @@
+"""
+Решить задачу Коши:
+
+    dx/dt = -x, x(0)=1, 0<t<3
+
+методом Эйлера первого порядка точности и
+методами Рунге-Кутты второго и четвертого порядка точности.
+
+ dx/dt = -x <-> dx/x = -t <-> x = exp(-t)
+
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+mint, maxt = 0, 3
+x0, t0 = 1, 0
+
+
+def difur(x):
+    res = -x
+    return res
+
+
+def func(t):
+    x = np.float64(np.exp(-t))
+    return x
+
+
+def euler():
+    pass
+
+def runge_kutta():
+    pass
+
+def run():
+    pass
