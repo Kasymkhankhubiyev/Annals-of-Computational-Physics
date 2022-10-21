82b70d9f0b3dca417e28c05dcd9476a90f7f2587
Kaseke <kasymkhankhubievnis@gmail.com>
2022-10-21 11:14:44 +0700

    started task8

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+from task1 import task1
 from task2 import task2
 from task3 import task3
+from task4 import task4
+from task5 import task5
+from task6 import task6
+from task7 import task7
+from task8 import task8
 
 if __name__ == '__main__':
 
