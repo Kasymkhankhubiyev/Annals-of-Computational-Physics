73cec0c6bcb2a69dd97dabb54d1b629fa88a774e
Kaseke <kasymkhankhubievnis@gmail.com>
2022-11-18 09:32:55 +0700

    Performed a signal visualization

--- task12/task12.py
+++ task12/task12.py
@@ -11,14 +11,17 @@ a0, a1 = 1., 0.002
 w0, w1 = 5.1, 25.5
 T = 2 * np.pi
 
-time = float
-signal = float
 
-
-def func(t: time) -> signal:
+def func(t: np.array) -> np.array:
     f = a0 * np.sin(w0 * t) + a1*np.sin(w1 * t)
     return f
 
 
 def run() -> None:
-    pass
+    time = np.linspace(0, T, 1000)
+    signal = func(time)
+    plt.plot(time, signal, color='green')
+    plt.legend('best')
+    plt.savefig('task12/signal_graph.png')
+    plt.close()
+
