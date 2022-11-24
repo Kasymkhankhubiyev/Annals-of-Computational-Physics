38ae83dfbb4c00f16c15fa05a5dfb3ee5078f02e
Kaseke <kasymkhankhubievnis@gmail.com>
2022-11-24 13:11:53 +0700

    done with task9

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from task9 import task9
 from task10 import task10
 from task11 import task11
-from task12 import task12
 
 if __name__ == '__main__':
 
-    task12.run()
+    task9.run()
--- task9/task9.py
+++ task9/task9.py
@@ -1,109 +1,66 @@
 """
-Методом погонки решить разностный аналог граничной задачи
+Методом прогонки решить разностный аналог граничной задачи
 для уравнения y''=cosx на промежутке -pi/2 < x < pi/2.
 
-TODO Рассмотреть различный варианты граничных условий (на функцию и ее производную).
-первого и второго рода
-
-y = cos(x) + bx + c
+y = -cos(x) + bx + c
 """
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-y0, yn = 0, 0  # граничные условия ->
-x0, xn = -np.pi/2, np.pi/2  # промежуток
 
-y_0, dy_0 = 0, 0  # граничные условия ->
-y_n = 0
+def _matrix(y0: float, yn: float, x0: float, xn: float, N: int):
+    d, x = [y0], [x0]
 
-N = 10000  # кол-во интервалов
-h = (xn - x0) / N  # шаг
+    a = np.ones(N+1)
+    b = np.zeros(N+1) - 2
+    c = np.ones(N+1)
 
-a0, b0, c0 = 0, -2, 1
+    a[0], a[N] = 0, 0
+    b[0], b[N] = 1, 1
+    c[0], c[N] = 0, 0
 
-
-def func(x):
-    return np.cos(x)
-
-
-def matrix():
-    a, b, c, d, x = [a0], [b0], [c0], [y0], [x0]
-
-    for i in range(1, N):
-        a.append(1)  # [a0, 1, 1, 1, ..., 1]
-        b.append(-2)  # [b0, -2, -2, -2, ..., -2]
-        c.append(1)  # [c0, 1, 1, 1, ..., 1]
-
-    a.append(1)
-    b.append(-2)
-    c.append(0)
+    h = (xn - x0) / N  # step
 
     for i in range(1, N):
         xi = x0 + h * i
-        d.append(h**2 * func(xi))
+        d.append(h**2 * np.cos(xi))
         x.append(xi)
 
-    d.append(yn)  # массив y_n
+    d.append(yn)
     x.append(xn)
-
-    # print(f'd matrix: {d}')
-    # print(f'x: {x}')
-
     return a, b, c, d, x
 
 
-def tridiagonal(a, b, c, d):
+def _tridiagonal(a, b, c, d, N):
     # прямой ход метода Гаусса - исключение поддиагональных элементов ai, i =2,..,n
     for i in range(1, N + 1):
         xi = a[i] / b[i - 1]  # a_0/b_1
         b[i] -= xi * c[i - 1]
         d[i] -= xi * d[i - 1]
 
-    # print(f'b: {b}')
-    # print(f'd: {d}')
-
     y = [i for i in range(N + 1)]
     y[N] = d[N] / b[N]
     """
-    y_n = d_n/b_n; x_i = (d_i - c_i * x_i+1)/b_i
+    $y_{n} = \frac{d_{n}}{b_{n}}; x_{i}=\frac{(d_{i} - c_{i}x_{i+1}}{b_{i}})$
     """
     for i in range(N - 1, -1, -1):
         y[i] = (d[i] - c[i] * y[i + 1]) / b[i]
-    # print(f'y: {y}')
-
     return y
 
 
-def solution(x):
-    c1 = (y0 - yn + func(x0) - func(xn)) / (x0 - xn)
-    c2 = y0 + func(x0) - c1 * x0
-    return -func(x) + c1 * x + c2
-
-def solution_2(x):
-    c1 = dy_0 - np.sin(x0)
-    c2 = y_0 + np.cos(x0) - c1 * x
-    return -func(x) + c1 * x + c2
-
-def solution_3(x):
-    c1 = dy_0 - np.sin(x0)
-    c2 = y_n + np.cos(xn) - c1 * x
-    return -func(x) + c1 * x + c2
+def _solution(x, y0, yn, x0, xn):
+    c1 = (y0 - yn + np.cos(x0) - np.cos(xn)) / (x0 - xn)
+    c2 = y0 + np.cos(x0) - c1 * x0
+    return -np.cos(x) + c1 * x + c2
 
 
-def run():
-    a, b, c, d, x = matrix()
-    y = tridiagonal(a, b, c, d)
-    sol = [solution(i) for i in x]
-    # error = [np.abs((y[i] - sol[i])) for i in range(len(x))]
-    error = np.abs(np.array(y) - np.array(sol))
-
+def _draw_solution(x, y, sol, error, y0, yn):
     fig = plt.figure(figsize=(10, 4))
 
     plt_gr = fig.add_subplot(121)
     plt_err = fig.add_subplot(122)
 
-    plt_gr.set_title('y(x)')
     plt_gr.set_xlabel('x')
     plt_err.set_ylabel('y')
     plt_gr.plot(x, y, color='red', label='calculated')
@@ -115,4 +72,27 @@ def run():
     fig.subplots_adjust(hspace=0.3, wspace=0.3)
     plt_gr.legend()
     plt_err.legend()
-    plt.savefig('task9/task9.png')
+    plt.savefig(f'task9/task9_y0={y0}_yn={yn}.png')
+
+
+def _solve_dif_equation(y0: float, yn: float, x0: float, xn: float, N: int):
+    a, b, c, d, x = _matrix(y0=y0, yn=yn, x0=x0, xn=xn, N=N)
+    y = _tridiagonal(a=a, b=b, c=c, d=d, N=N)
+    # sol = [_solution(i) for i in x]
+    sol = np.zeros_like(x)
+    sol += _solution(x=np.array(x), x0=x0, xn=xn, y0=y0, yn=yn)
+    error = np.abs(np.array(y) - np.array(sol))
+    _draw_solution(x, y, sol, error, y0, yn)
+
+
+def run():
+    # y0, yn = 0, 1  # граничные условия ->
+    x0, xn = -np.pi / 2, np.pi / 2  # промежуток
+
+    N = 1000  # кол-во интервалов
+
+    _solve_dif_equation(y0=0, yn=1, x0=x0, xn=xn, N=N)
+    _solve_dif_equation(y0=1, yn=0, x0=x0, xn=xn, N=N)
+    _solve_dif_equation(y0=1, yn=1, x0=x0, xn=xn, N=N)
+    _solve_dif_equation(y0=0, yn=0, x0=x0, xn=xn, N=N)
+    _solve_dif_equation(y0=-1, yn=1, x0=x0, xn=xn, N=N)
