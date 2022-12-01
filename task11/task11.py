6ee90d20f6cafb7464e09c623e64e414b5cc23b0
Kaseke <kasymkhankhubievnis@gmail.com>
2022-12-01 12:59:30 +0700

    commit changes

--- task11/task11.py
+++ task11/task11.py
@@ -7,7 +7,7 @@ import numpy as np
 """
 
 N = 30
-iter_number = 100
+iter_number = 10
 
 x0, xn = -15, 15
 h = (xn - x0) / N
@@ -45,21 +45,20 @@ def tridiagonal(d):
     a, b, c = matrix()
     d = d.copy()
 
-    for i in range(0, N):
+    for i in range(1, N):
         ksi = a[i] / b[i - 1]
         b[i] -= ksi * c[i - 1]
         d[i] -= ksi * d[i - 1]
 
     y = [i for i in range(N)]
     y[N - 1] = d[N - 1] / b[N - 1]
-
     for i in range(N - 2, -1, -1):
         y[i] = (d[i] - c[i] * y[i + 1]) / b[i]
 
     return y
 
 
-def inverse_iteration(psi_prev):
+def inverse_iteration(psi_prev):  # на первом шаге получаем вектор приближенных собственных значений
     for i in range(0, iter_number - 1):
         psi_prev = tridiagonal(psi_prev)
     psi = tridiagonal(psi_prev)
@@ -69,19 +68,43 @@ def inverse_iteration(psi_prev):
 
     return energy, psi
 
-
-def run():
-    energy, psi = inverse_iteration(np.array([0.7 for _ in range(N)]))
-    print(f'Calculated E0 = {energy}')
-    print(f'Real E0 = 0.5')
+def error_steps():
+    e_error, psi_error, nums = [], [], []
+    for n in range(30, 200, 10):
+        N = n
+        x_data = np.linspace(x0, xn, N)
+        print(n)
+        nums.append(n)
+        energy, psi = inverse_iteration(np.array([0.7 for _ in range(N)]))
+        analytic_solution = solution(x_data)
+        e_error.append(np.abs(energy-0.5))
+        psi_error.append(np.sum(np.abs(np.array(analytic_solution) - np.array(psi)))/len(analytic_solution))
 
     fig, axs = plt.subplots(nrows=1, ncols=2)
+    axs[0].plot(nums, e_error, label='energy error')
+    axs[1].plot(nums, psi_error, label='psi error')
+    axs[0].legend(fontsize=7, ncol=1, facecolor='oldlace', edgecolor='r')
+    axs[1].legend(fontsize=7, ncol=1, facecolor='oldlace', edgecolor='r')
 
-    x = np.linspace(x0, xn, N)
-    analytic_solution = solution(x)
+    plt.savefig('task11/task11_error_dependence.png')
 
-    axs[0].plot(x, analytic_solution, color='pink', label='real analytic_solution')
-    axs[0].plot(x, psi, color='purple', label=f'inverse iteration,\nN = {N},\niteration = {iter_number}')
-    axs[1].plot(x, analytic_solution - np.array(psi), color='blue')
 
-    plt.savefig('task11/task11.png')
+
+def run():
+    # energy, psi = inverse_iteration(np.array([0.7 for _ in range(N)]))
+    # print(f'Calculated E0 = {energy}')
+    # print(f'Real E0 = 0.5')
+    #
+    # fig, axs = plt.subplots(nrows=1, ncols=2)
+    #
+    # x = np.linspace(x0, xn, N)
+    # analytic_solution = solution(x)
+    #
+    # axs[0].plot(x, analytic_solution, color='pink', label='analytic')
+    # axs[0].plot(x, psi, color='purple', label=f'inverse iter')
+    # axs[1].plot(x, np.abs(analytic_solution - np.array(psi)), color='blue', label='error')
+    # axs[0].legend(fontsize=7, ncol=1, facecolor='oldlace', edgecolor='r')
+    # axs[1].legend(fontsize=7, ncol=1, facecolor='oldlace', edgecolor='r')
+    #
+    # plt.savefig(f'task11/task11_[{x0},{xn}].png')
+    error_steps()
