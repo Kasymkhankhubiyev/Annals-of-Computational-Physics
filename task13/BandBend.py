53cca2a5f789daef7a26ed1519dac4dd4774c322
Kaseke <kasymkhankhubievnis@gmail.com>
2022-12-05 22:27:49 +0700

    commit changes

--- task13/BandBend.py
+++ task13/BandBend.py
@@ -1,6 +1,11 @@
 import numpy as np
 from task13.exceptions import CantMatchMethod
 
+"""
+math: $\sqrt{\frac{\epsilon \phi_s N_d}{2 \pi e^2}} = N_as \frac{1}{1 + exp(\frac{Eas + \phi_s + E_f}{kT})} + 
+        \frac{E_{out}}{4\pi e}$
+"""
+
 
 def _dichotomy_method():
     return 0, 0
@@ -18,7 +23,9 @@ def _secant_method():
     pass
 
 
-def calculate_band_bend(method: str):
+def calculate_band_bend(Eg: float, epsilon: float, mc: float, mv: float, Ed: float, Nd: float, t: float,
+                        Nas: float, Eas: float, Eout: float, method: str):
+
     methods = ['dichotomy', 'newtown', 'fixed-point']
     bend, counter = None, 0
 
