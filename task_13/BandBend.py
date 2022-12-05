af153d86c2d0acd804099b16a082502d8e1c3d29
Kaseke <kasymkhankhubievnis@gmail.com>
2022-12-05 22:43:59 +0700

    commit changes

--- task_13/BandBend.py
+++ task_13/BandBend.py
@@ -2,12 +2,17 @@ import numpy as np
 from task_13.exceptions import CantMatchMethod
 
 
-def bend_function():
+def bend_function(epsilon: float, phi: float, Nd: float, Nas: float, Eas: float, Ef: float, t: float, Eout: float):
     """
     math: $\sqrt{\frac{\epsilon \phi_s N_d}{2 \pi e^2}} = N_as \frac{1}{1 + exp(\frac{Eas + \phi_s + E_f}{kT})} +
             \frac{E_{out}}{4\pi e}$
     """
-    return 0
+    k = 1  # Boltzmann constant
+    e = 1  # electron charge
+    n_as = Nas / (1. + np.exp((Eas + phi + Ef) / (k * t))) + Eout / (4 * np.pi * e)
+    w = (epsilon * phi * Nd / (2 * np.pi * e**2)) ** 0.5
+    return w - n_as
+
 
 def _dichotomy_method():
     return 0, 0
@@ -25,8 +30,8 @@ def _secant_method():
     pass
 
 
-def calculate_band_bend(Eg: float, epsilon: float, mc: float, mv: float, Ed: float, Nd: float, t: float,
-                        Nas: float, Eas: float, Eout: float, method: str, tolerance=1e-7):
+def calculate_band_bend(epsilon: float, Nd: float, t: float, Nas: float, Eas: float, Eout: float, method: str,
+                        tolerance=1e-7) -> tuple:
 
     methods = ['dichotomy', 'newtown', 'fixed-point']
     bend, counter = None, 0
